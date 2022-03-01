ed82399cad842f7908dab699229f33af730b2054
FxIvan <almendraivan210814@gmail.com>
2022-03-01 15:05:05 -0300

    probando un erutamiento

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react'
-import { Route , Switch } from 'react-router-dom'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import { Principal } from './component/principal/principal';
 
 function App() {
     return(
-        <Switch>
-          <Route path='/' component={Principal} exact/>
-        </Switch>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/ositoservicio' element={<Principal/>}/> 
+        </Routes>
+      </BrowserRouter>
     )
 }
 
