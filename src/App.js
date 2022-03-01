7852e47ff69df58777d76e2debf6723cc4a59082
FxIvan <almendraivan210814@gmail.com>
2022-03-01 11:08:39 -0300

    prueba 589485

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     return(
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Principal/>} />
+          <Route path='/' element={<Principal/>}/>
         </Routes>
       </BrowserRouter>
     )
