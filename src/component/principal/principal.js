622d467f06c3cde43edced5666ae8c84147a7d4f
FxIvan <almendraivan210814@gmail.com>
2022-03-01 01:45:01 -0300

    probando DEVUELTA

--- src/component/principal/principal.js
+++ src/component/principal/principal.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Principal = () =>{
     return(
         <div>
-            <h1>Esta es la pagina Principal</h1>
+            <h1>Esta es la pagina QUE HICE LA PRUEBA Y FUNCIONO!!</h1>
         </div>
     )
 }
