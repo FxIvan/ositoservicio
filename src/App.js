8bb4b87f314204a12873dee69bf315d728855aee
FxIvan <almendraivan210814@gmail.com>
2022-03-01 01:40:30 -0300

    probando la prueba

--- src/App.js
+++ src/App.js
@@ -4,14 +4,11 @@ import { Principal } from './component/principal/principal';
 
 function App() {
     return(
-      <h1>Esta es la Appjs</h1>
-      /*
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Principal/>} />
         </Routes>
       </BrowserRouter>
-      */
     )
 }
 
