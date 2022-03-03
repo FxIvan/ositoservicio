696e42fc2a06dffc65dc9d214ae0ca21a085ad31
FxIvan <almendraivan210814@gmail.com>
2022-03-03 20:30:13 -0300

    agregando seccion nosotros

--- src/component/navbar/navbar.js
+++ src/component/navbar/navbar.js
@@ -6,7 +6,7 @@ import logo from '../imgosito/logoPNG.png'
 
 export const Navbar = () =>{
     return(
-        <div className='container container-navbar-desktop d-flex col-md-12 pt-5 pb-5 text-align-center'>
+        <div className='container container-navbar-desktop d-flex col-md-12 pt-5 text-align-center'>
             <div className='navbar-web-desktop col-md-2'>
                 <Link to={'/'}><img src={logo} alt='logo navbar'/></Link>
             </div>
--- src/component/principal/principal.js
+++ src/component/principal/principal.js
@@ -1,12 +1,21 @@
 import React from 'react'
-import { Navbar } from '../navbar/navbar'
 import imgFondo from '../imgosito/portadaPNG.png'
+import nosotrosImg from '../imgosito/nosotrosLogo.png'
 
 export const Principal = () =>{
     return(
         <div>
             <div className='foto-portada'>
                 <img src={imgFondo}/>
+            </div>
+            <div>
+                <div className='text-center mt-4 pb-4'>
+                    <h2>Nosotros</h2>
+                </div>
+                <div className='container row m-auto nosotros-desktop mb-3'>
+                        <img src={nosotrosImg} className='text-center col-sm-3'/>    
+                        <p className='col-sm-9'>Somos un grupo de amigos que brinda servicio de intermediarias entre el consumidor y vendedor, evitando problemas con la seguridad y tiempo tanto para el consumidor como el vendedor, nuestra idea nació debido a que en la pandemia y antes de la pandemia las estafas online era recurrente para el consumidor que compraba online a través de la redes sociales(facebook, instagram, etc), resultaba que llegan a pagar el producto pero el producto no llega y terminan perdiendo dinero. Debido a este problema creamos un servicio donde el consumidor deposita el dinero en una cuenta perteneciente a servicios osito y cuando el producto llegue nosotros le depositamos al vendedor evitando estafas.</p>
+                </div>
             </div>
         </div>
     )
