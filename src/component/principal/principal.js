ceee2876aa122866de132084411422872204fe5f
FxIvan <almendraivan210814@gmail.com>
2022-03-05 00:26:31 -0300

    agregando hasta precio

--- src/component/principal/principal.js
+++ src/component/principal/principal.js
@@ -4,6 +4,9 @@ import nosotrosImg from '../imgosito/nosotrosLogo.png'
 import seguridad from '../imgosito/securityUndraw.svg'
 import rapidez from '../imgosito/speedUndraw.svg'
 import atencion from '../imgosito/atencionUndraw.svg'
+import precio250 from '../imgosito/250$.svg'
+import precio350 from '../imgosito/350$.svg'
+import { Link } from 'react-router-dom'
 export const Principal = () => {
   return (
     <div>
@@ -33,29 +36,91 @@ export const Principal = () => {
       </div>
       <div>
         <div>
-          <div className='container text-center mt-5 mb-3'>
+          <div className="container text-center mt-5 pb-5">
             <h2>TE BRINDAMOS</h2>
           </div>
-          <div className='undraw-desktop container row m-auto text-center'>
-            <div className='col-sm-4'>
-                <img src={seguridad} alt='seguridad'/>
-                <div className='pt-4'>    
-                    <span>SEGURIDAD</span>
-                </div>
+          <div className="undraw-desktop container row m-auto text-center">
+            <div className="col-sm-4">
+              <img src={seguridad} alt="seguridad" />
+              <div className="pt-4">
+                <span>SEGURIDAD</span>
+              </div>
             </div>
-            <div className='col-sm-4'>
-                <img src={rapidez} alt='rapidez'/>
-                <div className='pt-4'>
-                    <span>Rapidez</span>
-                </div>
+            <div className="col-sm-4">
+              <img src={rapidez} alt="rapidez" />
+              <div className="pt-4">
+                <span>Rapidez</span>
+              </div>
             </div>
-            <div className='col-sm-4'>
-                <img src={atencion} alt='atencion'/>
-                <div className='pt-4'>
-                    <span>Atencion <br/> <p>24/7</p></span>
-                </div>
+            <div className="col-sm-4">
+              <img src={atencion} alt="atencion" />
+              <div className="pt-4">
+                <span>
+                  Atencion <br /> <p>24/7</p>
+                </span>
+              </div>
             </div>
           </div>
+        </div>
+      </div>
+      <div>
+        <div className="precios-desktop">
+          <div className='text-center mt-5 mb-5'>
+            <h2>COSTO DEL SERVICIO</h2>
+          </div>
+<div className='container col-sm-12 row  m-auto'>
+          <div className='col-sm-4 m-auto'>
+            <div className='text-center'>
+              <img src={precio250} alt="precio" />
+            </div>
+            <div className='lista-precio-desktop-250'>
+              <ul>
+                <li>Atencion: Lunes a Viernes</li>
+                <li>Transacciones: 1</li>
+                <li>Duracion: 3 dias</li>
+                <li>Envios al exerior: SI</li>
+                <li>Moneda: ARS/ARS</li>
+              </ul>
+              <div className='text-center'>
+                <p>Horario de atencion 09:00 a 18:00</p>
+              </div>
+            </div>
+            <div className='text-center container-button-desktop'>
+              <Link to={'/costo/250'}  className='link-button-desktop'>
+                Contactar
+              </Link>
+            </div>
+          </div>
+
+          <div className='col-sm-4 m-auto'>
+            <div className='text-center'>
+              <img src={precio350} alt="precio" />
+            </div>
+            <div  className='lista-precio-desktop-350'>
+              <ul>
+                <li>
+                  <span>Atencion: 24/7</span>
+                </li>
+                <li>Transacciones: 1</li>
+                <li>
+                  <span>Duracion: 7 dias</span>
+                </li>
+                <li>Envios al exerior: SI</li>
+                <li>
+                  <span>Moneda: ARS/ARS y USD/USD</span>
+                </li>
+              </ul>
+              <div className='text-center'>
+                <p>Horario de atencion 24hs</p>
+              </div>
+            </div>
+            <div className='text-center container-button-desktop'>
+              <Link to={'/costo/350'} className='link-button-desktop'>
+                Contactar
+              </Link>
+            </div>
+          </div>
+          </div>{/******/}
         </div>
       </div>
     </div>
