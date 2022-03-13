cb4c441951ad32c8e865abbba23b33a30730bb75
FxIvan <almendraivan210814@gmail.com>
2022-03-13 20:01:44 -0300

    terminando formulario

--- src/component/formulario/formulario.js
+++ src/component/formulario/formulario.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../css/style.css'
-
+import axios from 'axios'
+import { Link, useHref } from 'react-router-dom'
 export const Formulario = () =>{
 
-    const handleChange = () =>{
-        
+    const [ form , setForm ] = useState({
+        nombre:'',
+        email:'',
+        telefono:'',
+        consulta:''
+    })
+
+    const handleChange = (e) =>{
+        setForm({
+            ...form,
+            [e.target.name]:e.target.value
+        })
+    }
+
+    const handleSubmit = async(e) =>{
+        e.preventDefault()
+        await axios.post('http://localhost:27017/formulario', form)
+        window.location.href='/'
     }
 
     return(
@@ -13,19 +30,19 @@ export const Formulario = () =>{
                 <h2>Dejanos tu consulta</h2>
             </div>
             <form className='col-sm-4 m-auto'>
-                <label for="formControlInput" class="form-label">Tu nombre</label>
-                <input type="text" className="form-control" id="formControlInput" placeholder="tu nombre" onChange={handleChange}/>
+                <label for="formControlInput" className="form-label">Tu nombre</label>
+                <input type="text" className="form-control" id="formControlInput" placeholder="tu nombre" onChange={handleChange} name='nombre'/>
 
-                <label for="formControlInput" class="form-label">Tu Email</label>
-                <input type="email" className="form-control" id="formControlInput" placeholder="tu email" onChange={handleChange} required/>
+                <label for="formControlInput" className="form-label">Tu Email</label>
+                <input type="email" className="form-control" id="formControlInput" placeholder="tu email" onChange={handleChange} required name='email'/>
 
-                <label for="formControlInput" class="form-label">Telefono de contacto</label>
-                <input type="number" className="form-control" id="formControlInput" placeholder="tu telefono" onChange={handleChange} required/>
+                <label for="formControlInput" className="form-label">Telefono de contacto</label>
+                <input type="number" className="form-control" id="formControlInput" placeholder="tu telefono" onChange={handleChange} required name='telefono'/>
 
-                <label for="formControlInput" class="form-label">Tu consulta</label>
-                <textarea type="text" className="form-control" id="formControlInput" placeholder="tu consulta" onChange={handleChange}/>
+                <label for="formControlInput" className="form-label">Tu consulta</label>
+                <textarea type="text" className="form-control" id="formControlInput" placeholder="tu consulta" onChange={handleChange} name='consulta'/>
                 <div className='text-center button-submit-desktop'>
-                    <input type="submit" value="Enviar" className='col-sm-2'/>
+                    <input type="submit" value="Enviar" className='col-sm-2' onClick={()=> handleSubmit}/>
                 </div>
             </form>
         </div>
